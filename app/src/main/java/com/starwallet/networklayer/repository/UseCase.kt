1a1632cee31ddc37dc638784c1e36a975053d837
Peter Fam <p.fam@starwallet.com>
2019-04-13 14:25:55 +0200

    applying new kotlin updates

--- app/src/main/java/com/starwallet/networklayer/repository/UseCase.kt
+++ app/src/main/java/com/starwallet/networklayer/repository/UseCase.kt
@@ -13,7 +13,7 @@ abstract class UseCase<out Type, in Params>(private val scope: CoroutineScope,
     //have to creat the job in the ViewModel to clear it after clearing the ViewModel
     abstract suspend fun run(params: Params): Either<Failure, Type>
 
-    operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) = {
+    operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}): () -> Job = {
         scope.launch {
             val result = run(params)
 
